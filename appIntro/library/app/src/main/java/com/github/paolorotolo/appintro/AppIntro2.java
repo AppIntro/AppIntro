c1c8f128b62b12adc1b8e16bf84da5145cfc70e7
Paolo Rotolo <paolorotolo@ubuntu.com>
2015-06-13 12:04:56 +0200

    Added new Layout.

---
+++ appIntro/library/app/src/main/java/com/github/paolorotolo/appintro/AppIntro2.java
@@ -0,0 +1,164 @@
+package com.github.paolorotolo.appintro;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
+public abstract class AppIntro2 extends FragmentActivity {
+    private PagerAdapter mPagerAdapter;
+    private ViewPager pager;
+    private List<Fragment> fragments = new Vector<Fragment>();
+    private List<ImageView> dots;
+    private int slidesNumber;
+    private Vibrator mVibrator;
+    private boolean isVibrateOn = false;
+    private int vibrateIntensity = 20;
+
+    private static int FIRST_PAGE_NUM = 0;
+
+    @Override
+    final protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.intro_layout2);
+
+        final ImageView nextButton = (ImageView) findViewById(R.id.next);
+        final ImageView doneButton = (ImageView) findViewById(R.id.done);
+        mVibrator = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
+
+        nextButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (isVibrateOn) {
+                    mVibrator.vibrate(vibrateIntensity);
+                }
+                pager.setCurrentItem(pager.getCurrentItem()+1);
+            }
+        });
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isVibrateOn) {
+                    mVibrator.vibrate(vibrateIntensity);
+                }
+                onDonePressed();
+            }
+        });
+
+        mPagerAdapter = new PagerAdapter(super.getSupportFragmentManager(), fragments);
+        pager = (ViewPager) findViewById(R.id.view_pager);
+        pager.setAdapter(this.mPagerAdapter);
+        pager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                selectDot(position);
+                if (position == slidesNumber - 1){
+                    nextButton.setVisibility(View.GONE);
+                    doneButton.setVisibility(View.VISIBLE);
+                } else {
+                    doneButton.setVisibility(View.GONE);
+                    nextButton.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+        init(savedInstanceState);
+        loadDots();
+    }
+
+    private void loadDots() {
+        LinearLayout dotLayout = (LinearLayout) findViewById(R.id.dotLayout);
+        dots = new ArrayList<>();
+        slidesNumber = fragments.size();
+
+        for (int i = 0; i < slidesNumber; i++) {
+            ImageView dot = new ImageView(this);
+            dot.setImageDrawable(getResources().getDrawable(R.drawable.indicator_dot_grey));
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+            );
+            dotLayout.addView(dot, params);
+
+            dots.add(dot);
+        }
+
+        selectDot(FIRST_PAGE_NUM);
+    }
+
+    public void selectDot(int index) {
+        Resources res = getResources();
+        for (int i = 0; i < fragments.size(); i++) {
+            int drawableId = (i == index) ? (R.drawable.indicator_dot_white) : (R.drawable.indicator_dot_grey);
+            Drawable drawable = res.getDrawable(drawableId);
+            dots.get(i).setImageDrawable(drawable);
+        }
+    }
+
+    public void addSlide(Fragment fragment, Context context) {
+        fragments.add(Fragment.instantiate(context, fragment.getClass().getName()));
+        mPagerAdapter.notifyDataSetChanged();
+    }
+
+    public void setVibrate(boolean vibrate){
+        this.isVibrateOn = vibrate;
+    }
+
+    public void setVibrateIntensity(int intensity){
+        this.vibrateIntensity = intensity;
+    }
+
+    public abstract void init(Bundle savedInstanceState);
+    public abstract void onDonePressed();
+
+    public class PagerAdapter extends FragmentPagerAdapter {
+
+        private List<Fragment> fragments;
+
+        public PagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+            super(fm);
+            this.fragments = fragments;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return this.fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return this.fragments.size();
+        }
+    }
+}
