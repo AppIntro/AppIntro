352ba6823efd4acc4e6c3748e2c095444c09cdfa
marbat87 <marbat87@outlook.it>
2015-07-17 10:46:05 +0200

    API-9 compatibility
    Added usage of nineoldandroids in order to extend compatibility to
    pre-Honeycomb

--- library/src/main/java/com/github/paolorotolo/appintro/FadePageTransformer.java
+++ library/src/main/java/com/github/paolorotolo/appintro/FadePageTransformer.java
@@ -6,17 +6,17 @@ import android.view.View;
 class FadePageTransformer implements ViewPager.PageTransformer {
     @Override
     public void transformPage(View view, float position) {
-        view.setTranslationX(view.getWidth() * -position);
+        ViewHelper.setTranslationX(view, view.getWidth() * -position);
 
         if (position <= -1.0F || position >= 1.0F) {
-            view.setAlpha(0.0F);
+            ViewHelper.setAlpha(view, 0.0F);
             view.setClickable(false);
         } else if (position == 0.0F) {
-            view.setAlpha(1.0F);
+            ViewHelper.setAlpha(view, 1.0F);
             view.setClickable(true);
         } else {
             // position is between -1.0F & 0.0F OR 0.0F & 1.0F
-            view.setAlpha(1.0F - Math.abs(position));
+            ViewHelper.setAlpha(view, 1.0F - Math.abs(position));
         }
     }
 }
