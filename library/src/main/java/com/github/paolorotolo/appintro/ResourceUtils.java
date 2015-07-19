45e61d31bfb049cdd8f671b68e22285576d957d2
porokoro <porokorodev@gmail.com>
2015-07-19 20:11:27 +0200

    Remove deprecation warning for Resources.getDrawable(int)

---
+++ library/src/main/java/com/github/paolorotolo/appintro/ResourceUtils.java
@@ -0,0 +1,19 @@
+package com.github.paolorotolo.appintro;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+class ResourceUtils {
+    @Nullable
+    static Drawable getDrawable(@NonNull Context context, @DrawableRes int drawableId) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            return context.getDrawable(drawableId);
+
+        //noinspection deprecation
+        return context.getResources().getDrawable(drawableId);
+    }
+}
