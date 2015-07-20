eddd1760bc44d4430c5f6cae9550f5333921ba65
Ben Butzow <ben@butzow.me>
2015-07-20 16:39:56 +0000

    Remove missed merge things

--- library/src/main/java/com/github/paolorotolo/appintro/AppIntro2.java
+++ library/src/main/java/com/github/paolorotolo/appintro/AppIntro2.java
@@ -6,15 +6,12 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
-<<<<<<< HEAD
 import android.support.v7.app.AppCompatActivity;
 import android.view.*;
-=======
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
->>>>>>> upstream/master
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
