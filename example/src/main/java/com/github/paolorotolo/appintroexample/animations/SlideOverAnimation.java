23800af38c49c55eb017c14f5961cc7a26932311
stormtheh4ck3r <rohitshampur09@yahoo.com>
2015-07-22 13:54:09 +0530

    Added new Animations into the example

---
+++ example/src/main/java/com/github/paolorotolo/appintroexample/animations/SlideOverAnimation.java
@@ -0,0 +1,47 @@
+package com.github.paolorotolo.appintroexample.animations;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintroexample.MainActivity;
+import com.github.paolorotolo.appintroexample.R;
+import com.github.paolorotolo.appintroexample.SampleSlide;
+
+/**
+ * Created by rohit on 22/7/15.
+ */
+public class SlideOverAnimation extends AppIntro {
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+        addSlide(SampleSlide.newInstance(R.layout.intro));
+        addSlide(SampleSlide.newInstance(R.layout.intro2));
+        addSlide(SampleSlide.newInstance(R.layout.intro3));
+        addSlide(SampleSlide.newInstance(R.layout.intro4));
+
+        setSlideOverAnimation();
+    }
+
+    private void loadMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onSkipPressed() {
+        loadMainActivity();
+        Toast.makeText(getApplicationContext(), getString(R.string.skip), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDonePressed() {
+        loadMainActivity();
+    }
+
+    public void getStarted(View v){
+        loadMainActivity();
+    }
+}
