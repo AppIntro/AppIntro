b9ab229380e1cb4490b0446dc55464490a4ec7fd
Salim KAYABAŞI <salimkayabasi@users.noreply.github.com>
2015-08-18 19:31:13 +0300

    add color options for title and desc

--- library/src/main/java/com/github/paolorotolo/appintro/AppIntroFragment.java
+++ library/src/main/java/com/github/paolorotolo/appintro/AppIntroFragment.java
@@ -12,54 +12,79 @@ import android.widget.TextView;
 
 public class AppIntroFragment extends Fragment {
 
-    private static final String ARG_TITLE = "title";
-    private static final String ARG_DESC = "desc";
-    private static final String ARG_DRAWABLE = "drawable";
-    private static final String ARG_COLOUR = "colour";
+  private static final String ARG_TITLE = "title";
+  private static final String ARG_DESC = "desc";
+  private static final String ARG_DRAWABLE = "drawable";
+  private static final String ARG_BG_COLOR = "bg_color";
+  private static final String ARG_TITLE_COLOR = "title_color";
+  private static final String ARG_DESC_COLOR = "desc_color";
 
-    public static AppIntroFragment newInstance(String title, String description, int imageDrawable, int colour) {
-        AppIntroFragment sampleSlide = new AppIntroFragment();
+  public static AppIntroFragment newInstance(String title, String description, int imageDrawable, int bgColor) {
+    AppIntroFragment sampleSlide = new AppIntroFragment();
 
-        Bundle args = new Bundle();
-        args.putString(ARG_TITLE, title);
-        args.putString(ARG_DESC, description);
-        args.putInt(ARG_DRAWABLE, imageDrawable);
-        args.putInt(ARG_COLOUR, colour);
-        sampleSlide.setArguments(args);
+    Bundle args = new Bundle();
+    args.putString(ARG_TITLE, title);
+    args.putString(ARG_DESC, description);
+    args.putInt(ARG_DRAWABLE, imageDrawable);
+    args.putInt(ARG_BG_COLOR, bgColor);
+    sampleSlide.setArguments(args);
 
-        return sampleSlide;
+    return sampleSlide;
+  }
+
+  public static AppIntroFragment newInstance(String title, String description, int imageDrawable, int bgColor, int titleColor, int descColor) {
+    AppIntroFragment sampleSlide = new AppIntroFragment();
+    Bundle args = new Bundle();
+    args.putString(ARG_TITLE, title);
+    args.putString(ARG_DESC, description);
+    args.putInt(ARG_DRAWABLE, imageDrawable);
+    args.putInt(ARG_BG_COLOR, bgColor);
+    args.putInt(ARG_TITLE_COLOR, titleColor);
+    args.putInt(ARG_DESC_COLOR, descColor);
+    sampleSlide.setArguments(args);
+
+    return sampleSlide;
+  }
+
+  private int drawable, bgColor, titleColor, descColor;
+  private String title, description;
+
+  public AppIntroFragment() {
+  }
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    if (getArguments() != null && getArguments().size() != 0) {
+      drawable = getArguments().getInt(ARG_DRAWABLE);
+      title = getArguments().getString(ARG_TITLE);
+      description = getArguments().getString(ARG_DESC);
+      bgColor = getArguments().getInt(ARG_BG_COLOR);
+      titleColor = getArguments().containsKey(ARG_TITLE_COLOR) ? getArguments().getInt(ARG_TITLE_COLOR) : 0;
+      descColor = getArguments().containsKey(ARG_DESC_COLOR) ? getArguments().getInt(ARG_DESC_COLOR) : 0;
     }
+  }
 
-    private int drawable, colour;
-    private String title, description;
-
-    public AppIntroFragment() {}
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if(getArguments() != null && getArguments().size() != 0){
-            drawable = getArguments().getInt(ARG_DRAWABLE);
-            title = getArguments().getString(ARG_TITLE);
-            description = getArguments().getString(ARG_DESC);
-            colour = getArguments().getInt(ARG_COLOUR);
-        }
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    View v = inflater.inflate(R.layout.fragment_intro, container, false);
+    TextView t = (TextView) v.findViewById(R.id.title);
+    TextView d = (TextView) v.findViewById(R.id.description);
+    ImageView i = (ImageView) v.findViewById(R.id.image);
+    LinearLayout m = (LinearLayout) v.findViewById(R.id.main);
+    t.setText(title);
+    if (titleColor != 0) {
+      t.setTextColor(titleColor);
     }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_intro, container, false);
-        TextView t = (TextView) v.findViewById(R.id.title);
-        TextView d = (TextView) v.findViewById(R.id.description);
-        ImageView i = (ImageView) v.findViewById(R.id.image);
-        LinearLayout m = (LinearLayout) v.findViewById(R.id.main);
-        t.setText(title);
-        d.setText(description);
-        i.setImageDrawable(ResourceUtils.getDrawable(getActivity(), drawable));
-        m.setBackgroundColor(colour);
-        return v;
+    d.setText(description);
+    if (descColor != 0) {
+      d.setTextColor(descColor);
     }
+    i.setImageDrawable(ResourceUtils.getDrawable(getActivity(), drawable));
+    m.setBackgroundColor(bgColor);
+    return v;
+  }
 
 }
