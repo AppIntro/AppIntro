19e3a8d1dd3bc7b42e951cf236f41ef069df2092
Sumeet Kumar <sumeetgehi@gmail.com>
2016-02-04 13:52:38 +0500

    updated sample app

--- example/src/main/java/com/github/paolorotolo/appintroexample/DefaultIntro.java
+++ example/src/main/java/com/github/paolorotolo/appintroexample/DefaultIntro.java
@@ -6,14 +6,25 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
 
 public class DefaultIntro extends AppIntro {
     @Override
     public void init(Bundle savedInstanceState) {
-        addSlide(SampleSlide.newInstance(R.layout.intro));
-        addSlide(SampleSlide.newInstance(R.layout.intro2));
-        addSlide(SampleSlide.newInstance(R.layout.intro3));
-        addSlide(SampleSlide.newInstance(R.layout.intro4));
+        // color must be resource ID
+        addSlide(AppIntroFragment.newInstance("Page 1", getResources().getString(R.string.app_about),
+				R.drawable.ic_launcher, R.color.primary_app), R.color.primary_app);
+		addSlide(AppIntroFragment.newInstance("Page 2", getResources().getString(R.string.app_about),
+				R.drawable.ic_launcher, R.color.primary_app_dark), R.color.primary_app_dark);
+		addSlide(AppIntroFragment.newInstance("Page 3", getResources().getString(R.string.about),
+				R.drawable.ic_launcher, R.color.red_error), R.color.red_error);
+		addSlide(AppIntroFragment.newInstance("Page 4", getResources().getString(R.string.about),
+				R.drawable.ic_launcher, R.color.accent2), R.color.accent2);
+		addSlide(AppIntroFragment.newInstance("Page 5", getResources().getString(R.string.about),
+				R.drawable.ic_launcher, R.color.accent), R.color.accent);
+				
+		int color = R.color.primary_app;
+		setNavBarColor(color);
     }
 
     private void loadMainActivity(){
