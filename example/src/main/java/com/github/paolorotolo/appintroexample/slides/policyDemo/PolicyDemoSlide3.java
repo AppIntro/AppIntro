88d235c41ff6a84885dd9bcb7e6042e665132073
Maximilian Narr <maxe.nr@live.de>
2016-04-15 15:12:32 +0200

    AppIntroBase & example app: Improved slide policy implementation and added policy demo to the example app

---
+++ example/src/main/java/com/github/paolorotolo/appintroexample/slides/policyDemo/PolicyDemoSlide3.java
@@ -0,0 +1,11 @@
+
+package com.github.paolorotolo.appintroexample.slides.policyDemo;
+
+import android.graphics.Color;
+
+public final class PolicyDemoSlide3 extends PolicyDemoSlide1 {
+    @Override
+    public int getDefaultBackgroundColor() {
+        return Color.parseColor("#FF9800");
+    }
+}
