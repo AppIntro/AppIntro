dc51fe922a25d11f9ea76810c5f22f4a97e59bb4
Timo Bähr <timo.baehr@gmail.com>
2016-04-16 14:57:06 +0200

    Skip button action for DefaultIntro2

--- example/src/main/java/com/github/paolorotolo/appintroexample/DefaultIntro2.java
+++ example/src/main/java/com/github/paolorotolo/appintroexample/DefaultIntro2.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.View;
+import android.widget.Toast;
 
 import com.github.paolorotolo.appintro.AppIntro2;
 
@@ -21,6 +22,13 @@ public final class DefaultIntro2 extends AppIntro2 {
     private void loadMainActivity(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        loadMainActivity();
+        Toast.makeText(getApplicationContext(), getString(R.string.skip), Toast.LENGTH_SHORT).show();
     }
 
     @Override
