a6db6833379bcb2e1280fa6aaadb1420d7899708
Rafael Gutiérrez <rafag@rafag.com>
2016-06-02 22:36:11 +0200

    Set visibility of Skip button

--- library/src/main/java/com/github/paolorotolo/appintro/AppIntroBase.java
+++ library/src/main/java/com/github/paolorotolo/appintro/AppIntroBase.java
@@ -316,8 +316,15 @@ public abstract class AppIntroBase extends AppCompatActivity implements AppIntro
         // ;
     }
 
-    public void setSkipButtonEnabled(boolean skipButtonEnabled) {
-        this.skipButtonEnabled = skipButtonEnabled;
+    /**
+     * Setting this to display or hide the Skip button. This is a static setting and
+     * button state is maintained across slides until explicitly changed.
+     *
+     * @param showButton Set true to display. false to hide.
+    */
+    public void showSkipButton(boolean showButton) {
+        this.skipButtonEnabled = showButton;
+        setButtonState(skipButton, showButton);
     }
 
     public boolean isSkipButtonEnabled() {
