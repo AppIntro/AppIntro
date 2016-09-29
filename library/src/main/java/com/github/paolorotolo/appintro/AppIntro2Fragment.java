c752f7005912e5dfce27c178fca29c82fbfe186d
Toni Lozancic <toni.lozancic@swissfm.ch>
2016-09-29 14:13:09 +0200

    Library now supports image url for loading image dynamically from the web. Loading is handled from Picasso library

--- library/src/main/java/com/github/paolorotolo/appintro/AppIntro2Fragment.java
+++ library/src/main/java/com/github/paolorotolo/appintro/AppIntro2Fragment.java
@@ -29,6 +29,29 @@ public final class AppIntro2Fragment extends AppIntroBaseFragment {
         return slide;
     }
 
+    public static AppIntroFragment newInstance(CharSequence title, CharSequence description,
+                                               String drawableUri, @ColorInt int bgColor) {
+        return newInstance(title, description, drawableUri, bgColor, 0, 0);
+    }
+
+    public static AppIntroFragment newInstance(CharSequence title, CharSequence description,
+                                               String drawableUri, @ColorInt int bgColor,
+                                               @ColorInt int titleColor, @ColorInt int descColor) {
+        AppIntroFragment slide = new AppIntroFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_TITLE, title.toString());
+        args.putString(ARG_TITLE_TYPEFACE, null);
+        args.putString(ARG_DESC, description.toString());
+        args.putString(ARG_DESC_TYPEFACE, null);
+        args.putString(ARG_DRAWABLE_URI, drawableUri);
+        args.putInt(ARG_BG_COLOR, bgColor);
+        args.putInt(ARG_TITLE_COLOR, titleColor);
+        args.putInt(ARG_DESC_COLOR, descColor);
+        slide.setArguments(args);
+
+        return slide;
+    }
+
     public static AppIntroFragment newInstance(CharSequence title, String titleTypeface,
                                                CharSequence description, String descTypeface,
                                                @DrawableRes int imageDrawable,
@@ -48,6 +71,33 @@ public final class AppIntro2Fragment extends AppIntroBaseFragment {
         args.putString(ARG_DESC, description.toString());
         args.putString(ARG_DESC_TYPEFACE, descTypeface);
         args.putInt(ARG_DRAWABLE, imageDrawable);
+        args.putInt(ARG_BG_COLOR, bgColor);
+        args.putInt(ARG_TITLE_COLOR, titleColor);
+        args.putInt(ARG_DESC_COLOR, descColor);
+        slide.setArguments(args);
+
+        return slide;
+    }
+
+    public static AppIntroFragment newInstance(CharSequence title, String titleTypeface,
+                                               CharSequence description, String descTypeface,
+                                               String drawableUri,
+                                               @ColorInt int bgColor) {
+        return newInstance(title, titleTypeface, description, descTypeface, drawableUri, bgColor,
+                0, 0);
+    }
+
+    public static AppIntroFragment newInstance(CharSequence title, String titleTypeface,
+                                               CharSequence description, String descTypeface,
+                                               String drawableUri, @ColorInt int bgColor,
+                                               @ColorInt int titleColor, @ColorInt int descColor) {
+        AppIntroFragment slide = new AppIntroFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_TITLE, title.toString());
+        args.putString(ARG_TITLE_TYPEFACE, titleTypeface);
+        args.putString(ARG_DESC, description.toString());
+        args.putString(ARG_DESC_TYPEFACE, descTypeface);
+        args.putString(ARG_DRAWABLE_URI, drawableUri);
         args.putInt(ARG_BG_COLOR, bgColor);
         args.putInt(ARG_TITLE_COLOR, titleColor);
         args.putInt(ARG_DESC_COLOR, descColor);
