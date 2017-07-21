7d478246e40a16d6ab3306c4e8d9d93cc4db125f
David <david@nustechnology.com>
2017-07-21 10:08:28 +0700

    change libs to 4.2.0

--- example/src/main/java/com/amqtech/opensource/appintroexample/ui/permsTabs/intro/PermissionsIntro2.java
+++ example/src/main/java/com/amqtech/opensource/appintroexample/ui/permsTabs/intro/PermissionsIntro2.java
@@ -6,9 +6,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 
+import com.amqtech.opensource.appintroexample.SliderPageModel;
 import com.github.paolorotolo.appintro.AppIntro2;
 import com.github.paolorotolo.appintro.AppIntroFragment;
-import com.github.paolorotolo.appintro.model.SliderPage;
 import com.github.paolorotolo.appintroexample.R;
 
 /**
@@ -21,36 +21,41 @@ public class PermissionsIntro2 extends AppIntro2 {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        SliderPage sliderPage1 = new SliderPage();
+        SliderPageModel sliderPage1 = new SliderPageModel();
         sliderPage1.setTitle( "Welcome!" );
         sliderPage1.setDescription( "This is a demo of the AppIntro library, with a custom background on each slide!" );
         sliderPage1.setImageDrawable( R.drawable.ic_slide1 );
         sliderPage1.setBgColor( Color.TRANSPARENT );
-        addSlide( AppIntroFragment.newInstance(sliderPage1));
+        add(sliderPage1);
 
-        SliderPage sliderPage2 = new SliderPage();
+        SliderPageModel sliderPage2 = new SliderPageModel();
         sliderPage2.setTitle( "Clean App Intros" );
         sliderPage2.setDescription( "This library offers developers the ability to add clean app intros at the start of their apps." );
         sliderPage2.setImageDrawable(  R.drawable.ic_slide2 );
         sliderPage2.setBgColor( Color.TRANSPARENT );
-        addSlide(AppIntroFragment.newInstance(sliderPage2));
+        add(sliderPage2);
 
-        SliderPage sliderPage3 = new SliderPage();
+        SliderPageModel sliderPage3 = new SliderPageModel();
         sliderPage3.setTitle( "Simple, yet Customizable" );
         sliderPage3.setDescription( "The library offers a lot of customization, while keeping it simple for those that like simple." );
         sliderPage3.setImageDrawable(  R.drawable.ic_slide3 );
         sliderPage3.setBgColor( Color.TRANSPARENT );
-        addSlide(AppIntroFragment.newInstance(sliderPage3));
+        add(sliderPage3);
 
-        SliderPage sliderPage4 = new SliderPage();
+        SliderPageModel sliderPage4 = new SliderPageModel();
         sliderPage4.setTitle( "Explore" );
         sliderPage4.setDescription( "Feel free to explore the rest of the library demo!" );
         sliderPage4.setImageDrawable(  R.drawable.ic_slide4 );
         sliderPage4.setBgColor( Color.TRANSPARENT );
-        addSlide(AppIntroFragment.newInstance(sliderPage4));
+        add(sliderPage4);
         // Here we load a string array with a camera permission, and tell the library to request permissions on slide 2
         askForPermissions(new String[]{Manifest.permission.CAMERA}, 2);
     }
+
+    public void add(SliderPageModel slide){
+        addSlide(AppIntroFragment.newInstance(slide.getTitle(),slide.getDescription(),slide.getImageDrawable(),slide.getBgColor()));
+    }
+
 
     @Override
     public void onSkipPressed(Fragment currentFragment) {
