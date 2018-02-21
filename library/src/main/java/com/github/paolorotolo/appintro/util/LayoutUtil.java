2d8544867f5b1a40a2fbbd4054c24cbf126bd164
Efi Merdler-Kravitz <efi@coneuron.com>
2018-02-21 12:22:19 +0200

    Get RTL status the right way

--- library/src/main/java/com/github/paolorotolo/appintro/util/LayoutUtil.java
+++ library/src/main/java/com/github/paolorotolo/appintro/util/LayoutUtil.java
@@ -2,12 +2,20 @@ package com.github.paolorotolo.appintro.util;
 
 import android.content.res.Resources;
 import android.os.Build;
+import android.text.TextUtils;
 import android.view.View;
+
+import java.util.Locale;
 
 public class LayoutUtil {
 
+    @Deprecated
     public static boolean isRtl(Resources resources) {
+        return isRtl();
+    }
+
+    public static boolean isRtl() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1
-                && resources.getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL;
+                && TextUtils.getLayoutDirectionFromLocale(Locale.getDefault()) == View.LAYOUT_DIRECTION_RTL;
     }
 }
