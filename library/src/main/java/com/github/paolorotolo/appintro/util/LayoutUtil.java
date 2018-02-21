1932ef0a0bbb8287acf957f18024e18d43be95b2
Efi Merdler-Kravitz <efi@coneuron.com>
2018-02-21 13:34:58 +0200

    Use a different mechanism for RTL check

--- library/src/main/java/com/github/paolorotolo/appintro/util/LayoutUtil.java
+++ library/src/main/java/com/github/paolorotolo/appintro/util/LayoutUtil.java
@@ -3,31 +3,32 @@ package com.github.paolorotolo.appintro.util;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.content.res.Resources;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.text.TextUtils;
 import android.view.View;
 
 import java.util.Locale;
 
-import static android.content.pm.ApplicationInfo.FLAG_SUPPORTS_RTL;
-
 public class LayoutUtil {
 
     public static boolean isRtl(@NonNull Context ctx) {
-        try {
-            ApplicationInfo info = ctx.getPackageManager().getApplicationInfo(ctx.getPackageName(), PackageManager.GET_META_DATA);
-
-            return defaultIsRtlBehavior();
-        } catch (PackageManager.NameNotFoundException e) {
-            return defaultIsRtlBehavior();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            try {
+                ApplicationInfo info = ctx.getPackageManager().getApplicationInfo(ctx.getPackageName(), PackageManager.GET_META_DATA);
+                return (info.flags &= ApplicationInfo.FLAG_SUPPORTS_RTL) != 0;
+            } catch (PackageManager.NameNotFoundException e) {
+                return defaultIsRtlBehavior();
+            }
         }
+
+        return false;
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     private static boolean defaultIsRtlBehavior() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1
-                && TextUtils.getLayoutDirectionFromLocale(Locale.getDefault()) == View.LAYOUT_DIRECTION_RTL;
+        return TextUtils.getLayoutDirectionFromLocale(Locale.getDefault()) == View.LAYOUT_DIRECTION_RTL;
     }
 }
