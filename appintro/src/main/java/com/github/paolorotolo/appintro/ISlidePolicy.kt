20b8e818d008ac6087bfc168c485f7f462e1b81c
Nicola Corti <nco@yelp.com>
2018-10-28 08:55:42 -0700

    Convert all the interfaces to Kotlin

--- appintro/src/main/java/com/github/paolorotolo/appintro/ISlidePolicy.kt
+++ appintro/src/main/java/com/github/paolorotolo/appintro/ISlidePolicy.kt
@@ -1,17 +1,17 @@
-package com.github.paolorotolo.appintro;
+package com.github.paolorotolo.appintro
 
-public interface ISlidePolicy {
+interface ISlidePolicy {
 
     /**
      * Whether the user has fulfilled the slides policy and should be allowed to navigate through the intro further.
-     * If false is returned, {@link #onUserIllegallyRequestedNextPage()} will be called.
+     * If false is returned, [.onUserIllegallyRequestedNextPage] will be called.
      *
      * @return True if the user should be allowed to leave the slide, else false.
      */
-    boolean isPolicyRespected();
+    val isPolicyRespected: Boolean
 
     /**
      * Called if a user tries to go to the next slide while into navigation has been locked.
      */
-    void onUserIllegallyRequestedNextPage();
+    fun onUserIllegallyRequestedNextPage()
 }
