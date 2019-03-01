dc96c72c6a5ebc9991686137205ded04b157757e
Anurag Thakur <devanuragthakur@gmail.com>
2019-03-01 22:30:42 +0530

    Fix crash on lower API versions

--- appintro/src/main/java/com/github/paolorotolo/appintro/internal/LayoutUtil.kt
+++ appintro/src/main/java/com/github/paolorotolo/appintro/internal/LayoutUtil.kt
@@ -11,7 +11,11 @@ internal object LayoutUtil {
 
     @JvmStatic
     fun isRtl(ctx: Context): Boolean {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 &&
-                ctx.resources.configuration.layoutDirection == View.LAYOUT_DIRECTION_RTL
+       var isRTL : Boolean
+        isRTL = false //RTL is supported only on API 17, so it will be false on API<17
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            isRTL = ctx.resources.configuration.layoutDirection == View.LAYOUT_DIRECTION_RTL
+        }
+        return isRTL
     }
 }
