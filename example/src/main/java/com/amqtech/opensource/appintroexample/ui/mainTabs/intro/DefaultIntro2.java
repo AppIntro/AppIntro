a6bb70dee2220f4e01aa33f9ae04dbdc4e43bf4e
Anurag Thakur <devanuragthakur@gmail.com>
2019-03-02 22:18:19 +0530

    Made ColorFul

--- example/src/main/java/com/amqtech/opensource/appintroexample/ui/mainTabs/intro/DefaultIntro2.java
+++ example/src/main/java/com/amqtech/opensource/appintroexample/ui/mainTabs/intro/DefaultIntro2.java
@@ -24,14 +24,14 @@ public class DefaultIntro2 extends AppIntro2 {
         sliderPage1.setTitle("Welcome!");
         sliderPage1.setDescription("This is a demo of the AppIntro library, using the second layout.");
         sliderPage1.setImageDrawable(R.drawable.ic_slide1);
-        sliderPage1.setBgColor(Color.TRANSPARENT);
+        sliderPage1.setBgColor(Color.parseColor("#2196F3"));
         addSlide(AppIntroFragment.newInstance(sliderPage1));
 
         SliderPage sliderPage2 = new SliderPage();
         sliderPage2.setTitle("Clean App Intros");
         sliderPage2.setDescription("This library offers developers the ability to add clean app intros at the start of their apps.");
         sliderPage2.setImageDrawable(R.drawable.ic_slide2);
-        sliderPage2.setBgColor(Color.TRANSPARENT);
+        sliderPage2.setBgColor(Color.parseColor("#009688"));
         addSlide(AppIntroFragment.newInstance(sliderPage2));
 
         SliderPage sliderPage3 = new SliderPage();
@@ -40,14 +40,14 @@ public class DefaultIntro2 extends AppIntro2 {
         sliderPage3.setTitleTypefaceFontRes(R.font.opensans_regular);
         sliderPage3.setDescTypefaceFontRes(R.font.opensans_regular);
         sliderPage3.setImageDrawable(R.drawable.ic_slide3);
-        sliderPage3.setBgColor(Color.TRANSPARENT);
+        sliderPage3.setBgColor(Color.parseColor("#FFC107"));
         addSlide(AppIntroFragment.newInstance(sliderPage3));
 
         SliderPage sliderPage4 = new SliderPage();
         sliderPage4.setTitle("Explore");
         sliderPage4.setDescription("Feel free to explore the rest of the library demo!");
         sliderPage4.setImageDrawable(R.drawable.ic_slide4);
-        sliderPage4.setBgColor(Color.TRANSPARENT);
+        sliderPage4.setBgColor(Color.parseColor("#D32F2F"));
         addSlide(AppIntroFragment.newInstance(sliderPage4));
     }
 
