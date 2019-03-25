54102a0d9b9e01935b9fffc2f3d3e8a93ec30cb5
Ivan Iskandar <ivan@prjkt.io>
2019-03-25 11:24:34 +0700

    TypefaceContainer: Use non-blocking method of getting font resource
    Signed-off-by: Ivan Iskandar <ivan@prjkt.io>

--- appintro/src/main/java/com/github/paolorotolo/appintro/internal/CustomFontCache.kt
+++ appintro/src/main/java/com/github/paolorotolo/appintro/internal/CustomFontCache.kt
@@ -2,6 +2,7 @@ package com.github.paolorotolo.appintro.internal
 
 import android.content.Context
 import android.graphics.Typeface
+import androidx.core.content.res.ResourcesCompat
 
 /**
  * Custom Font Cache Implementation.
@@ -12,21 +13,20 @@ internal object CustomFontCache {
     private val TAG = LogHelper.makeLogTag(CustomFontCache::class)
     private val cache = hashMapOf<String, Typeface>()
 
-    operator fun get(path: String?, ctx: Context): Typeface? {
+    fun getFont(ctx: Context, path: String?, fontCallback: ResourcesCompat.FontCallback) {
         if (path.isNullOrEmpty()) {
             LogHelper.w(TAG, "Empty typeface path provided!")
-            return null
+            return
         }
 
-        val storedTypeface = cache[path]
-        return if (storedTypeface != null) {
+        cache[path]?.let {
             // Cache hit! Return the typeface.
-            storedTypeface
-        } else {
+            fontCallback.onFontRetrieved(it)
+        } ?: run {
             // Cache miss! Create the typeface and store it.
             val newTypeface = Typeface.createFromAsset(ctx.assets, path)
-            cache[path!!] = newTypeface
-            newTypeface
+            cache[path] = newTypeface
+            fontCallback.onFontRetrieved(newTypeface)
         }
     }
 }
