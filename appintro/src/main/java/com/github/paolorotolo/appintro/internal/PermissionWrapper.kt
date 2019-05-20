02860cea60e78b5598359cac2b814d19d561e08c
Nicola Corti <corti.nico@gmail.com>
2019-05-20 23:23:21 +0200

    Support requesting permissions without locking the Swipe
    Make sure we don't have to `setSwipeLock(true)` if the user is
    requesting a permission. This is achieved by requesting the permission
    during the `onPageScrolled`. If the permission is denied, AppIntro will
    go to the previous slide.
    
    Fix #676

--- appintro/src/main/java/com/github/paolorotolo/appintro/internal/PermissionWrapper.kt
+++ appintro/src/main/java/com/github/paolorotolo/appintro/internal/PermissionWrapper.kt
@@ -1,15 +1,16 @@
 package com.github.paolorotolo.appintro.internal
 
-import java.util.*
-
 /**
  * A data class that represents a set of permissions that should be requested to the user.
  * @property permissions An Array of Permissions from the Android Framework
  * @property position The position in the AppIntro pager when to request those permissions.
+ * @property pending Flag set to true if AppIntro already requested this permission and is awaiting response.
  */
 internal data class PermissionWrapper(
         var permissions: Array<String>,
-        var position: Int) {
+        var position: Int,
+        internal var pending: Boolean = false
+) {
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -17,15 +18,17 @@ internal data class PermissionWrapper(
 
         other as PermissionWrapper
 
-        if (!Arrays.equals(permissions, other.permissions)) return false
+        if (!permissions.contentEquals(other.permissions)) return false
         if (position != other.position) return false
+        if (pending != other.pending) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        var result = Arrays.hashCode(permissions)
+        var result = permissions.contentHashCode()
         result = 31 * result + position
+        result = 31 * result + pending.hashCode()
         return result
     }
 }
