62a6ed025aee193b91258a984e367576ea72dfc7
AnuthaDev <anuthadev@gmail.com>
2020-01-16 17:36:45 +0530

    Update Dependencies

--- appintro/src/main/java/com/github/paolorotolo/appintro/internal/viewpager/PagerAdapter.kt
+++ appintro/src/main/java/com/github/paolorotolo/appintro/internal/viewpager/PagerAdapter.kt
@@ -1,41 +1,17 @@
 package com.github.paolorotolo.appintro.internal.viewpager
 
-import android.util.SparseArray
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
 internal class PagerAdapter(
     fragmentManager: FragmentManager,
-    val fragments: List<Fragment>
-) : FragmentPagerAdapter(fragmentManager) {
+    private val fragments: List<Fragment>
+) : FragmentPagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
-    private val retainedFragments: SparseArray<Fragment> = SparseArray()
-
-    override fun getItem(position: Int): Fragment? {
-        return if (!fragments.isEmpty()) {
-            // Check if the fragment at this position has been retained by the PagerAdapter
-            if (retainedFragments.get(position) != null)
-                retainedFragments.get(position)
-            else
-                fragments[position]
-        } else null
+    override fun getItem(position: Int): Fragment {
+        return fragments[position]
     }
 
     override fun getCount() = this.fragments.size
-
-    override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val fragment = super.instantiateItem(container, position) as Fragment
-        retainedFragments.put(position, fragment)
-
-        return fragment
-    }
-
-    override fun destroyItem(container: ViewGroup, position: Int, view: Any) {
-        if (retainedFragments.get(position) != null) {
-            retainedFragments.remove(position)
-        }
-        super.destroyItem(container, position, view)
-    }
 }
