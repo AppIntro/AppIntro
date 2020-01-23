d3bbe7e3f903861c9f1bbcdc6ea0c28fd0ab801b
Nicola Corti <corti.nico@gmail.com>
2020-01-23 21:19:44 +0100

    Kotlinify AppIntroCustomLayoutSlide

--- appintro/src/main/java/com/github/paolorotolo/appintro/AppIntroCustomLayoutSlide.kt
+++ appintro/src/main/java/com/github/paolorotolo/appintro/AppIntroCustomLayoutSlide.kt
@@ -6,28 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class CustomSlide : Fragment() {
+class AppIntroCustomLayoutSlide : Fragment() {
+
     private var layoutResId = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (arguments != null && arguments!!.containsKey(ARG_LAYOUT_RES_ID)) {
-            layoutResId = arguments!!.getInt(ARG_LAYOUT_RES_ID)
-        }
+        layoutResId = arguments?.getInt(ARG_LAYOUT_RES_ID) ?: 0
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(layoutResId, container, false)
-    }
+    ): View? = inflater.inflate(layoutResId, container, false)
 
     companion object {
         private const val ARG_LAYOUT_RES_ID = "layoutResId"
         @JvmStatic
-        fun newInstance(layoutResId: Int): CustomSlide {
-            val customSlide = CustomSlide()
+        fun newInstance(layoutResId: Int): AppIntroCustomLayoutSlide {
+            val customSlide = AppIntroCustomLayoutSlide()
             val args = Bundle()
             args.putInt(ARG_LAYOUT_RES_ID, layoutResId)
             customSlide.arguments = args
