a19e65e56b3d0d47efe5e22f1e547a2053603bb3
AnuthaDev <anuthadev@gmail.com>
2020-01-30 01:05:43 +0530

    Update MaterialDrawer

--- example/src/main/java/com/github/appintro/example/ui/MainActivity.kt
+++ example/src/main/java/com/github/appintro/example/ui/MainActivity.kt
@@ -2,13 +2,16 @@ package com.github.appintro.example.ui
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.github.appintro.appintroexample.R
 import com.github.appintro.example.ui.fragment.MainTabsFragment
 import com.github.appintro.example.ui.fragment.PermissionTabsFragment
+import com.mikepenz.materialdrawer.Drawer
 import com.mikepenz.materialdrawer.DrawerBuilder
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -34,17 +37,19 @@ class MainActivity : AppCompatActivity() {
                                 .withIdentifier(2)
                                 .withName("Permissions")
                 )
-                .withOnDrawerItemClickListener {_, _, drawerItem ->
-                    var fragment: Fragment = MainTabsFragment()
+                .withOnDrawerItemClickListener(object: Drawer.OnDrawerItemClickListener{
+                    override fun onItemClick(view: View?, position: Int, drawerItem: IDrawerItem<*>): Boolean {
+                        var fragment: Fragment = MainTabsFragment()
 
-                    when (drawerItem.identifier.toInt()) {
-                        1 -> fragment = MainTabsFragment()
-                        2 -> fragment = PermissionTabsFragment()
+                        when (drawerItem.identifier.toInt()) {
+                            1 -> fragment = MainTabsFragment()
+                            2 -> fragment = PermissionTabsFragment()
+                        }
+
+                        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
+                        return false
                     }
-
-                    supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
-                    false
-                }
+                })
                 .withShowDrawerOnFirstLaunch(true)
                 .withFireOnInitialOnClick(true)
                 .withSavedInstance(savedInstanceState)
