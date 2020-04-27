fa419912cc0ec39c90ea56d7c4e1663babb495a9
Nicola Corti <corti.nico@gmail.com>
2020-04-27 21:40:04 +0200

    Fix broken GH Actions build

--- example/src/main/java/com/github/appintro/example/ui/mainTabs/intro/DefaultIntro2.kt
+++ example/src/main/java/com/github/appintro/example/ui/mainTabs/intro/DefaultIntro2.kt
@@ -16,15 +16,15 @@ class DefaultIntro2 : AppIntro2() {
                 "Welcome!",
                 "This is a demo of the AppIntro library, using the second layout.",
                 imageDrawable = R.drawable.ic_slide1,
-                bgDrawable = R.drawable.back_slide1,
+                backgroundDrawable = R.drawable.back_slide1,
                 titleTypefaceFontRes = R.font.lato,
-                descTypefaceFontRes = R.font.lato
+                descriptionTypefaceFontRes = R.font.lato
         ))
 
         addSlide(AppIntroFragment.newInstance(SliderPage(
                 "Gradients!",
                 "This text is written on a gradient background",
-                imageDrawable =R.drawable.ic_slide2,
+                imageDrawable = R.drawable.ic_slide2,
                 backgroundDrawable = R.drawable.back_slide3,
                 titleTypeface = "OpenSans-Light.ttf",
                 descriptionTypeface = "OpenSans-Light.ttf"
@@ -34,17 +34,17 @@ class DefaultIntro2 : AppIntro2() {
         addSlide(AppIntroFragment.newInstance(
                 "Simple, yet Customizable",
                 "The library offers a lot of customization, while keeping it simple for those that like simple.",
-                imageDrawable =R.drawable.ic_slide3,
-                bgDrawable = R.drawable.back_slide4,
+                imageDrawable = R.drawable.ic_slide3,
+                backgroundDrawable = R.drawable.back_slide4,
                 titleTypefaceFontRes = R.font.opensans_regular,
-                descTypefaceFontRes = R.font.opensans_regular
+                descriptionTypefaceFontRes = R.font.opensans_regular
         ))
 
         addSlide(AppIntroFragment.newInstance(
                 "Explore",
                 "Feel free to explore the rest of the library demo!",
-                imageDrawable =R.drawable.ic_slide4,
-                bgDrawable = R.drawable.back_slide5
+                imageDrawable = R.drawable.ic_slide4,
+                backgroundDrawable = R.drawable.back_slide5
         ))
 
         setTransformer(AppIntroPageTransformerType.Parallax())
