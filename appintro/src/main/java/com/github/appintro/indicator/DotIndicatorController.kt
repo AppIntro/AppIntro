ee5de5072921401be74d325f7972c97d00397b67
Debanshu Datta <debanshudatta123@gmail.com>
2020-10-06 10:39:09 +0530

    Adding changes to DotIndicatorController.kt

--- appintro/src/main/java/com/github/appintro/indicator/DotIndicatorController.kt
+++ appintro/src/main/java/com/github/appintro/indicator/DotIndicatorController.kt
@@ -46,6 +46,7 @@ class DotIndicatorController(context: Context) : IndicatorController, LinearLayo
         this.slideCount = slideCount
 
         for (i in 0 until slideCount) {
+            if (slideCount == 1) { break }
             val dot = ImageView(this.context)
             dot.setImageDrawable(
                 ContextCompat.getDrawable(this.context, R.drawable.ic_appintro_indicator)
@@ -67,7 +68,9 @@ class DotIndicatorController(context: Context) : IndicatorController, LinearLayo
             } else {
                 unselectedIndicatorColor
             }
-            (getChildAt(i) as ImageView).let { DrawableCompat.setTint(it.drawable, tint) }
+            if (slideCount == 1) { break } else {
+                (getChildAt(i) as ImageView).let { DrawableCompat.setTint(it.drawable, tint) }
+            }
         }
     }
 }
