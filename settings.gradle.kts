f2763413b1853f9c0fdfe1163636d7b88ff72627
Nicola Corti <corti.nico@gmail.com>
2022-09-13 23:22:15 +0100

    Move the build to Gradle KTS and Version Catalogs

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+include(":appintro", ":example")
