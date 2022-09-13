e75b07ca338d55beb6c4b026744a71d4acf0ce31
Nicola Corti <corti.nico@gmail.com>
2022-09-13 23:22:15 +0100

    Move the build to Gradle KTS and Version Catalogs

---
+++ appintro/build.gradle.kts
@@ -0,0 +1,108 @@
+plugins {
+    alias(libs.plugins.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.ktlint)
+    id("maven-publish")
+}
+
+group = "com.github.AppIntro"
+version = "6.2.0"
+
+android {
+    namespace = "com.github.appintro"
+    compileSdk = libs.versions.compile.sdk.version.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.min.sdk.version.get().toInt()
+        targetSdk = libs.versions.target.sdk.version.get().toInt()
+
+        consumerProguardFiles("consumer-proguard-rules.pro")
+        vectorDrawables.useSupportLibrary = true
+    }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+    lint {
+        warningsAsErrors = true
+        abortOnError = true
+        lint {
+            disable.addAll(
+                listOf(
+                    "MissingTranslation",
+                    "OldTargetApi",
+                    "GradleDependency"
+                )
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.constraintlayout)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.mockito.core)
+}
+
+ktlint {
+    debug.set(false)
+    verbose.set(true)
+    android.set(false)
+    outputToConsole.set(true)
+    ignoreFailures.set(false)
+}
+
+detekt {
+    config = files("./detekt-config.yml")
+}
+
+val POM_NAME: String by project
+val POM_DESCRIPTION: String by project
+val POM_LICENSE_NAME: String by project
+val POM_LICENSE_URL: String by project
+val POM_SCM_CONNECTION: String by project
+val POM_URL: String by project
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            afterEvaluate {
+                from(components["release"])
+            }
+            pom {
+                name.set(POM_NAME)
+                description.set(POM_DESCRIPTION)
+                url.set(POM_URL)
+                licenses {
+                    license {
+                        name.set(POM_LICENSE_NAME)
+                        url.set(POM_LICENSE_URL)
+                    }
+                }
+                scm {
+                    connection.set(POM_SCM_CONNECTION)
+                    developerConnection.set(POM_SCM_CONNECTION)
+                    url.set(POM_URL)
+                }
+                developers {
+                    developer {
+                        id.set("paolorotolo")
+                        name.set("Paolo Rotolo")
+                        email.set("paolo@rotolo.dev")
+                    }
+                    developer {
+                        id.set("cortinico")
+                        name.set("Nicola Corti")
+                        email.set("corti.nico@gmail.com")
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ example/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    alias(libs.plugins.application)
+    kotlin("android")
+}
+
+android {
+    compileSdk = libs.versions.compile.sdk.version.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.min.sdk.version.get().toInt()
+        targetSdk = libs.versions.target.sdk.version.get().toInt()
+
+        vectorDrawables.useSupportLibrary = true
+        applicationId = "com.github.appintro.example"
+    }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+    lint {
+        disable.addAll(
+            listOf(
+                "MissingTranslation",
+                "OldTargetApi",
+                "GradleDependency"
+            )
+        )
+    }
+}
+
+dependencies {
+    implementation(project(":appintro"))
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.cardview)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.core.ktx)
+}
