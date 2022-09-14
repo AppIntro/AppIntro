74fb9b9677ddd68aa0e6af0df97c87b67be346b1
Nicola Corti <corti.nico@gmail.com>
2022-09-14 14:11:50 +0100

    Move the build to Gradle KTS and Version Catalogs (#1030)

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    alias(libs.plugins.application) apply false
+    alias(libs.plugins.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.ktlint) apply false
+}
