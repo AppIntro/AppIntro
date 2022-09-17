9d2d0f1894ca33ed2deddfc2677066c5c22eed76
Nicola Corti <corti.nico@gmail.com>
2022-09-17 20:39:40 +0100

    Removed unused import

--- appintro/src/main/java/com/github/appintro/AppIntroBase.kt
+++ appintro/src/main/java/com/github/appintro/AppIntroBase.kt
@@ -10,7 +10,6 @@ import android.view.KeyEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
-import android.view.WindowManager
 import android.widget.ImageButton
 import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
