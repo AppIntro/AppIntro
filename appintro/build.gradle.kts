24ec65c5de846f11e767515b510e5a4d739fc7eb
Nicola Corti <corti.nico@gmail.com>
2023-02-19 20:34:43 +0100

    jvmTarget to 11

--- appintro/build.gradle.kts
+++ appintro/build.gradle.kts
@@ -39,6 +39,13 @@ android {
             )
         }
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
 }
 
 dependencies {
--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -29,6 +29,13 @@ android {
             )
         )
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
 }
 
 dependencies {
