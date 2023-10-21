90b48702570f2fb26a19344c59266cdb81813c8f
Felix Nüsse <felix.nuesse@t-online.de>
2023-10-21 16:10:13 +0200

    add convenience-wrapper

--- appintro/src/main/java/com/github/appintro/AppIntroBase.kt
+++ appintro/src/main/java/com/github/appintro/AppIntroBase.kt
@@ -156,15 +156,27 @@ abstract class AppIntroBase : AppCompatActivity(), AppIntroViewPagerListener {
         }
     }
 
+
+    /**
+     * Adds a new slide at the end of the Intro and associates the permissions with it
+     * @param fragment Instance of Fragment which should be added as slide.
+     * @param permissions - Array of permissions
+     * @param required - Whether the user can change this slide without granting the permissions.
+     */
+    protected fun addSlideWithPermissionRequest(fragment: Fragment, permissions: Array<String>, required: Boolean = true) {
+        addSlide(fragment)
+        askForPermissions(permissions, fragment, required)
+    }
+
     /**
      * Called by the user to associate permissions with slides.
      *
      * @param permissions - Array of permissions
-     * @param AppIntroFragment - The slide at which permission is to be asked.
+     * @param slide - The Fragment at which permission is to be asked.
      * @param required - Whether the user can change this slide without granting the permissions.
      */
     @JvmOverloads
-    protected fun askForPermissions(permissions: Array<String>, slide: AppIntroFragment, required: Boolean = true) {
+    protected fun askForPermissions(permissions: Array<String>, slide: Fragment, required: Boolean = true) {
         val slideNumber = fragments.indexOf(slide)+1
         askForPermissions(permissions, slideNumber, required)
     }
